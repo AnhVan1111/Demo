0b622a5deb55a7dbfda8c4974ebe253be6b79d76
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-10-08 19:57:35 +0700

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Demo
+Face Detection demo
--- data/test_images.py
+++ data/test_images.py
@@ -1,1 +1,30 @@
-import cv2 as cv;
+import cv2
+import cv2.data
+import matplotlib.pyplot as plt
+# Load the image
+image_path = "c:/code/GrProj/Demo/path/image.jpg"
+img = cv2.imread(image_path)
+
+if img is None:
+    print("Error: Could not read image.")
+    exit()
+
+# Convert to grayscale
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Load the Haar Cascade for face detection
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Detect faces
+faces = faceCascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
+
+# Draw rectangles around detected faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+# Display the image using matplotlib
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis('off')  # Hide axes
+plt.show()
+
+
